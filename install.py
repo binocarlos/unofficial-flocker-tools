5ae1ea164dfec6e260e38d569203894d9bbb6e69
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-02 18:11:52 +0100

    deal with line length but putting a comment as the first line

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     c = Configurator(configFile=sys.argv[1])
     for node in c.config["agent_nodes"]:
         if c.config["os"] == "ubuntu":
-            c.runSSH(node, """
+            c.runSSH(node, """# install flocker
 apt-get -y install apt-transport-https software-properties-common
 add-apt-repository -y ppa:james-page/docker
 add-apt-repository -y 'deb https://clusterhq-archive.s3.amazonaws.com/ubuntu-testing/14.04/$(ARCH) /' # noqa
@@ -20,7 +20,8 @@ apt-get update
 apt-get -y --force-yes install clusterhq-flocker-node
 """)
         elif c.config["os"] == "centos":
-            c.runSSH(node, """if selinuxenabled; then setenforce 0; fi
+            c.runSSH(node, """# install flocker
+if selinuxenabled; then setenforce 0; fi
 test -e /etc/selinux/config && \
 sed --in-place='.preflocker' \
 's/^SELINUX=.*$/SELINUX=disabled/g' /etc/selinux/config
@@ -33,8 +34,9 @@ yum install -y clusterhq-flocker-node
         # CentOS ZFS installation requires a restart
         # XXX todo - find out a way to handle a restart mid-script
         if c.config["os"] == "centos":
-            print """Auto-install of ZFS on centos not currently supported - 
-            restart required"""
+            print >> sys.stderr, """Auto-install of ZFS on centos is
+            not currently supported"""
+            sys.exit(1)
 
         for node in c.config["agent_nodes"]:
             if c.config["os"] == "ubuntu":
